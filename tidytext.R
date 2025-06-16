d7a106dc577ab5323a9c611567beeeee605e333c
milkyamy <55010790+milkyamy@users.noreply.github.com>
2025-06-16 00:26:52 -0400

    Add files via upload

--- tidytext.R
+++ tidytext.R
@@ -1,23 +1,26 @@
 library(readxl)
-text <- read_excel("D:/Amy Su/Cornell/Graduate research/1 - Researching children in the digital age/2_Data/text.xlsx")
+library(dplyr)
+library(tidytext)
+library(ggplot2)
+library(wordcloud)
+library(reshape2)
+library(textdata)
+
+text <- read_excel("C:/Users/Amy Su/Desktop/Amy Su/Research/1 - Researching children in the digital age/2_Data/text.xlsx")
 View(text)
 
 #TidyTextDataset(p.2-3)#
-
-library(dplyr)
-text_df <- data_frame(line=1:29, text = text)
-text_df
+#text_df <- tibble(line=1:29, text = text)#
+#text_df#
 
 #Tokenization(p.3)#
 
-library(tidytext)
-text_df %>%  
+text_df <- text %>%
   unnest_tokens(word, text)
 
 #WordFrequencies(p.6-7)#
 
 dim(text_df[text_df$word=="online",])
-
 
 data("stop_words")
 
@@ -26,8 +29,6 @@ text_df <- text_df %>%
 
 text_df %>%
   count(word, sort = TRUE)
-
-library(ggplot2)
 
 text_df %>%
   count(word, sort = TRUE) %>% View()
@@ -38,34 +39,40 @@ text_df %>%
   labs(y = NULL)
 
 #Wordclouds(p.25)#
-library(wordcloud)
+
+custom_stop_words <- tibble(word = c("e.g", "i.e"))  
+all_stop_words <- stop_words %>%
+  bind_rows(custom_stop_words)
+
+text_df_clean <- text_df %>%
+  anti_join(all_stop_words, by = "word")
 
 text_df %>%
-  anti_join(stop_words) %>%
-  count(word) %>%
-  with(wordcloud(word, n, max.words = 100))
+    anti_join(all_stop_words) %>%
+    count(word) %>%
+    with(wordcloud(word, n, max.words = 80))
 
 #Wordclouds+SentimentAnalysis(p.26)#
-
-library(reshape2)
 
 text_df %>%
   inner_join(get_sentiments("bing")) %>%
   count(word, sentiment, sort = TRUE) %>%
   acast(word ~ sentiment, value.var = "n", fill = 0) %>%
   comparison.cloud(colors = c("gray20", "gray80"),
-                   max.words = 100)
+                   max.words = 80,
+                   scale = c(3, 0.5),         # reduce font size
+                   random.order = FALSE)      # center big words first
 
 #SentimentAnlysis(p.17)#
-library(textdata)
-nrc_joy <- get_sentiments("nrc") %>% 
-  filter(sentiment == "joy")
 
-nrc_pos <- get_sentiments("nrc") %>% 
-  filter(sentiment == "positive")
+#nrc_joy <- get_sentiments("nrc") %>%#
+  #filter(sentiment == "joy")#
 
-nrc_neg <- get_sentiments("nrc") %>% 
-  filter(sentiment == "negative")
+#nrc_pos <- get_sentiments("nrc") %>%# 
+  #filter(sentiment == "positive")#
+
+#nrc_neg <- get_sentiments("nrc") %>%# 
+  #filter(sentiment == "negative")#
 
 text_df %>%
   inner_join(nrc_joy) %>%
